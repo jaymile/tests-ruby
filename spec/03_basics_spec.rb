bb24e2250c1a9ed301ad7cfdfa75fb1e92ad812c
jiaad <abdulziad08@outlook.com>
2019-01-14 22:58:51 +0100

    finale

--- spec/03_basics_spec.rb
+++ spec/03_basics_spec.rb
@@ -31,29 +31,29 @@ describe '42 finder' do
   # - false otherwise
   # Hint: Should be 2 lines (and can be one :)
   # Hint: google ruby array each
-  it 'finds 42' do
-    expect(array_42([1, 2, 3, 4, 5, 6, 7 , 8, 9, 10])).to eq false
-    expect(array_42([1, 2, 3, 4, 5, 6, 7 , 8, 9, 42, 21, 10.5])).to eq true
-  end
-end
-#
-#
-# describe 'crazy stuff on arrays' do
-#   # The magic_array function takes an array of number or an array of
-#   # array of number as parameter and return the same array :
-#   # - flattened (i.e. no more arrays in array)
-#   # - sorted
-#   # - with each number multiplicated by 2
-#   # - with each multiple of 3 removed
-#   # - with each number duplicate removed (any number should appear only once)
-#   # - sorted
-#   # BONUS : You can do this in one line less than 55 chars
-#   it 'does crazy stuff on Arrays' do
-#     expect(magic_array([1, 2, 3, 4, 5, 6]))
-#       .to eq([2, 4, 8, 10])
-#     expect(magic_array([1, [2, 3], 4, 5, 6, 23, 31, [1, 2, 3]]))
-#       .to eq([2, 4, 8, 10, 46, 62])
-#     expect(magic_array([[32, 54], [48, 12], [21, [1, 2, [3]]], 7, 8]))
-#       .to eq([2, 4, 14, 16, 64])
+#   it 'finds 42' do
+#     expect(array_42([1, 2, 3, 4, 5, 6, 7 , 8, 9, 10])).to eq false
+#     expect(array_42([1, 2, 3, 4, 5, 6, 7 , 8, 9, 42, 21, 10.5])).to eq true
 #   end
 # end
+#
+
+describe 'crazy stuff on arrays' do
+  # The magic_array function takes an array of number or an array of
+  # array of number as parameter and return the same array :
+  # - flattened (i.e. no more arrays in array)
+  # - sorted
+  # - with each number multiplicated by 2
+  # - with each multiple of 3 removed
+  # - with each number duplicate removed (any number should appear only once)
+  # - sorted
+  # BONUS : You can do this in one line less than 55 chars
+  it 'does crazy stuff on Arrays' do
+    expect(magic_array([1, 2, 3, 4, 5, 6]))
+      .to eq([2, 4, 8, 10])
+    expect(magic_array([1, [2, 3], 4, 5, 6, 23, 31, [1, 2, 3]]))
+      .to eq([2, 4, 8, 10, 46, 62])
+    expect(magic_array([[32, 54], [48, 12], [21, [1, 2, [3]]], 7, 8]))
+      .to eq([2, 4, 14, 16, 64])
+  end
+end
